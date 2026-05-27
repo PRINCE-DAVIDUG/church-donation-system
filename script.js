29ea0958afc636ca1ab8360947560db85b0e8b46
PRINCE-DAVIDUG <princedavidpehaug@gmail.com>
2026-05-27 23:59:19 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+const form = document.getElementById('donationForm');
+
+form.addEventListener('submit', async (e) => {
+
+  e.preventDefault();
+
+  const donationData = {
+    fullname: document.getElementById('fullname').value,
+    email: document.getElementById('email').value,
+    phone: document.getElementById('phone').value,
+    donationType: document.getElementById('donationType').value,
+    amount: document.getElementById('amount').value,
+    message: document.getElementById('message').value
+  };
+
+  const response = await fetch('http://localhost:5000/api/donations/donate', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(donationData)
+  });
+
+  const result = await response.json();
+
+  if(result.success) {
+    window.location.href = result.payment_url;
+  } else {
+    alert('Payment initialization failed');
+  }
+});
